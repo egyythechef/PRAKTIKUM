70c043aad90bad84ed21938c40513f4014ec149b
egyythechef <shiddiq811@gmail.com>
2026-09-24 18:11:24 +0800

    Add files via upload

---
+++ PRAK002/PRAK002-2610820310016-AzkaMaulanaAshShiddiq.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char nama[] = "Azka";
+
+    printf("Selamat Pagi, %s\n", nama);
+    printf("Selamat Siang, %s\n", nama);
+    printf("Selamat Malam, %s\n", nama);
+
+    return 0;
+}
---
+++ PRAK004/PRAK004-2610820310016-AzkaMaulanaAshShiddiq.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("##############################\n");
+    printf("#                            #\n");
+    printf("#           Azka             #\n");
+    printf("#        2610820310016       #\n");
+    printf("#                            #\n");
+    printf("##############################\n");
+
+    return 0;
+}
